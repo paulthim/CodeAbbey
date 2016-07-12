784751e11dbe651feba4d118648f8545331f9341
Paul <paul@Pauls-MacBook-Pro-2.local>
2016-07-12 15:26:00 -0400

    test commit with Java Task 1

---
+++ Java/Task1/sumOfTwo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class sumOfTwo {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		int b = in.nextInt();
+		System.out.println(a+b);
+		in.close();
+    }
+}
